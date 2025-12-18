75068904cfbeaba14f35ef0159e4863ea0d60d89
Khun <lykhun@gmail.com>
2025-12-18 14:06:17 +0100

    line

---
+++ Scribbl.Net/Models/Line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scribbl.Net.Models
+{
+    public class Line
+    {
+        public double FromX { get; set; }
+        public double FromY { get; set; }
+        public double ToX { get; set; }
+        public double ToY { get; set; }
+        public string Color { get; set; } = null!;
+        public int Thickness { get; set; }
+    }
+}
